9769839b8b81be539aad4c8a57631f9a0bd28be6
SuperAuMan <makejack4@gmail.com>
2018-08-06 22:53:58 +0700

    change endpoint

--- server/client/src/components/CustomerMonitors/CustomerDisplay.js
+++ server/client/src/components/CustomerMonitors/CustomerDisplay.js
@@ -10,7 +10,7 @@ class CustomerDisplay extends Component {
       showprice: "",
       status: -1,
       grandtotal: 0,
-      endpoint: "http://localhost:5000"
+      endpoint: "http://127.0.0.1:5000"
     };
   }
   componentDidMount() {
